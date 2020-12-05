769207b5c1f77aaeca4e8d0bbe59aab88666d648
Khalyomede <khalyomede@gmail.com>
2020-12-05 10:48:00 +0100

    #1 fix wrong exported TS type (which fixes #1)

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 /**
  * @see https://blogs.ancestry.com/cm/calling-james-smith-10-most-common-first-and-surname-combinations/
  */
-declare const emails: String[];
+declare const emails: string[];
 export { emails, };
